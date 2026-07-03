65f266fcb07894d39b413ce44f8cabf57c3b8c6f
Tahmid-Arik <tahmidbinismail285@gmail.com>
2026-07-03 23:41:02 +0600

    Add files via upload

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ load_dotenv(DOTENV_PATH)
 # Fetch single active API key
 api_key = os.getenv("GEMINI_API_KEY")
 
-st.title("Health and Medical Care App-SciBlitz 2026")
-st.subheader("Welcome to our Health & Medical service.Tell me about your physical condition and I will try to provide you with the best possible answer based on the information available in my database[Hypertension,Coronary Artery,Stroke,Heart Failure,Palmonary Artery,Palmonary Embolism]")
+st.title("Health & Medical Care AI Assistant ")
+st.subheader("Welcome to our Health & Medical service. To assist you, please describe your current physical condition. Our database provides detailed information on cardiovascular and pulmonary diseases[Hypertension, Coronary Artery Disease, Stroke, Heart Failure, Pulmonary Artery Disease, and Pulmonary Embolisms]")
 
 DATA_FILE_PATH = os.path.join(BASE_DIR, "data", "disease_info.txt")
 
@@ -48,15 +48,17 @@ col1, col2,col3 = st.columns(3)
 with col1:
     
     existing_disease = st.selectbox("Did you have any disease in previous?", ["None", "Hypertension", "Coronary Artery Disease", "Stroke", "Heart Failure", "Palmonary Artery Disease", "Palmonary Embolism"])
-
     user_weight = st.number_input("your weight in kg:", min_value=1.0, max_value=200.0, value=70.0,step=0.1)
+    user_bp_high = st.number_input("BP High (Systolic):", min_value=50, max_value=250, value=120, step=1)
 with col2:
-    symptom_days = st.slider("How many days you are suffering? (Days)", 1, 14, 3)
+    symptom_days = st.number_input("How many days you are suffering?", min_value=1, max_value=100, value=3, step=1)
     user_height = st.number_input("your height in cm:", min_value=1.0, max_value=250.0, value=170.0,step=0.1)
+    user_bp_low = st.number_input("BP Low (Diastolic):", min_value=30, max_value=150, value=80, step=1)
 with col3:
     user_age = st.number_input("your age:", min_value=1, max_value=100, value=25,step=1)
     user_gender = st.selectbox("your gender:", ["Male", "Female",])
-
+    user_blood_group = st.selectbox("your blood group:", ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"])
+    
 user_bmi, bmi_category, user_bmr = calculate_metrics(user_weight, user_height, user_age, user_gender)
 
 if st.button("Calculate BMI and BMR"):
@@ -88,14 +90,12 @@ if st.button("Answer"):
                 6.Height: {user_height} cm
                 7.BMI: {user_bmi:.2f} ({bmi_category})
                 8.BMR: {user_bmr:.2f} kcal/day
+                9.Blood Pressure: {user_bp_high}/{user_bp_low} mmHg
+                10.Blood Group: {user_blood_group}[only if query related to blood transfusion or donation]
 
-                Evaluation Rules:You must consider all the user profile information variable provided above when evaluating the user's symptoms and providing advice.
-                if user ask any general health?medical information provide him/her only within the context of {context_data} and if the user's question is not relatable with  {context_data}
-                if the user's question is not relatable with  {context_data},then inform him about that with an apology and give some general health advice .
+                Evaluation Rules:You must consider all the user profile information variable provided above when evaluating the user's symptoms and providing advice.Validate user symptoms semantically against the {context_data}; if a symptom's core meaning is absent or has no direct medical match in the {context_data}, explicitly state you cannot find it in the database and offer only general advice
                 Crucial: Always end with a medical disclaimer."""
 
-
-                
                 try:  
                     genai.configure(api_key=api_key)
                     model = genai.GenerativeModel(model_name="gemini-2.5-flash")
